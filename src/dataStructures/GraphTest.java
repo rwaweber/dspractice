85b96609a1ebed5084dbffe0e4f3617c3f6fc67c
rwaweber <rwaweber@udel.edu>
2016-02-21 23:45:51 -0500

    adding test stubs

---
+++ src/dataStructures/GraphTest.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package dataStructures;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author demos
+ *
+ */
+public class GraphTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link dataStructures.Graph#Graph(java.util.ArrayList)}.
+	 */
+	@Test
+	public void testGraphArrayListOfNodeOfT() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link dataStructures.Graph#insert(dataStructures.Node)}.
+	 */
+	@Test
+	public void testInsertNodeOfT() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link dataStructures.Graph#insert(dataStructures.Node, dataStructures.Node)}.
+	 */
+	@Test
+	public void testInsertNodeOfTNodeOfT() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link dataStructures.Graph#connect(dataStructures.Node, dataStructures.Node)}.
+	 */
+	@Test
+	public void testConnect() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link dataStructures.Graph#retrieve(java.lang.String)}.
+	 */
+	@Test
+	public void testRetrieve() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link dataStructures.Graph#remove(java.lang.String)}.
+	 */
+	@Test
+	public void testRemoveString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link dataStructures.Graph#remove(dataStructures.Node)}.
+	 */
+	@Test
+	public void testRemoveNodeOfT() {
+		fail("Not yet implemented");
+	}
+
+}
